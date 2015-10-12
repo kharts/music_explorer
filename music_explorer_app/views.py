3de8edcec9d518ab58cf8b3ecfe1fa4a8abf96b9
kharts <kharts@ukr.net>
2015-10-12 04:37:31 -0700

    Add copyright notice to music_explorer_app/views.py

--- music_explorer_app/views.py
+++ music_explorer_app/views.py
@@ -1,3 +1,12 @@
+# music_explorer_app/views.py - definitions of views of music_explorer_app
+#
+# Copyright 2015 kharts (https://github.com/kharts)
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+__author__ = 'kharts'
+
 from django.http import HttpResponse
 
 def index(request):
